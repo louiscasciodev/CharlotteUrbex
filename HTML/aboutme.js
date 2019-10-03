264b6efc198ec3fc45c00bdc654a36b23d7a412c
Mickaelchoipro <mickaelchoipro@gmail.com>
2019-10-03 13:15:08 +0200

    form corrigé + contact avec icones

---
+++ HTML/aboutme.js
@@ -0,0 +1,21 @@
+
+
+/*console.log('qfgqlifgqy')
+
+const openModal = document.getElementById('open-modal');
+const modal = document.getElementById('modal');
+
+openModal.addEventListener('click', () => {
+  modal.style.display = 'block';
+})
+
+const closeModal = document.getElementById('close-modal');
+
+closeModal.addEventListener('click', () => {
+  modal.style.display = 'none';
+})*/
+
+const yolo = (e) => {
+    e.preventDefault();
+    alert('vkegflewhgvs')
+  }
