a65b2db773b3bd3995ec0983852478892909e7af
Mickaelchoipro <mickaelchoipro@gmail.com>
2019-10-04 09:44:08 +0200

    modal

--- HTML/aboutme.js
+++ HTML/aboutme.js
@@ -1,21 +1,16 @@
-
-
-/*console.log('qfgqlifgqy')
-
 const openModal = document.getElementById('open-modal');
 const modal = document.getElementById('modal');
 
-openModal.addEventListener('click', () => {
+console.log(modal);
+
+const openModalFunction = openModal.addEventListener('click', () => {
+  event.preventDefault();
   modal.style.display = 'block';
 })
 
 const closeModal = document.getElementById('close-modal');
 
 closeModal.addEventListener('click', () => {
+  event.preventDefault();
   modal.style.display = 'none';
-})*/
-
-const yolo = (e) => {
-    e.preventDefault();
-    alert('vkegflewhgvs')
-  }
+})
